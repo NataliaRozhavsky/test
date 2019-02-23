a240a0ccb344d666bb7f7af8a5d7a9f31bf753bd
NataliaRozhavsky <natalia.rozhavsky@gmail.com>
2019-02-23 14:08:07 +0200

    commit test

--- src/counter.js
+++ src/counter.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './counter.scss';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-// comment
+// comment 2
 class Counter extends Component {
 	constructor(props) {
 		super(props);
